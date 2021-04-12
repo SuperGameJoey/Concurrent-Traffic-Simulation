cb150ae25adced79f2ccc60653a62fff1a3b4d22
Joey <jlapegna@gmail.com>
2021-04-12 23:48:02 +0000

    completed final tasks

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -12,16 +12,27 @@
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
+    // COMPLETE! FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _condition.wait(uLock, [this] { return !_messages.empty(); });
+
+    T msg = std::move(_messages.back());
+    _messages.pop_back();
+
+    return msg;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
+    // COMPLETE! FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+
+    std::lock_guard<std::mutex> lock(_mutex);
+    _messages.push_back(std::move(msg));
+    _condition.notify_one();
 }
 
 
@@ -35,9 +46,14 @@ TrafficLight::TrafficLight()
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
+    // COMPLETE! FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while (true)
+    {
+        TrafficLightPhase phase = _queue.receive();
+        if (phase == TrafficLightPhase::green) return;
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -63,7 +79,7 @@ void TrafficLight::cycleThroughPhases()
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
 
     // create monitor object as a shared pointer
-    std::shared_ptr<MessageQueue<TrafficLightPhase>> queue(new MessageQueue<TrafficLightPhase>);
+    //std::shared_ptr<MessageQueue<TrafficLightPhase>> queue(new MessageQueue<TrafficLightPhase>);
     
     // store current time as start time
     auto startTime = std::chrono::system_clock::now();
@@ -88,7 +104,9 @@ void TrafficLight::cycleThroughPhases()
             startTime = std::chrono::system_clock::now();
 
             // send a message to the queue
-            std::async(std::launch::async, &MessageQueue<TrafficLightPhase>::send, queue, std::move(_currentPhase));
+            //std::async(std::launch::async, &MessageQueue<TrafficLightPhase>::send, queue, std::move(_currentPhase));
+            TrafficLightPhase phaseMsg = _currentPhase;
+            _queue.send(std::move(phaseMsg));
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
