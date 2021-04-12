27a0da0ad0515f016924f934f667c8f96610d2dc
Joey <jlapegna@gmail.com>
2021-04-12 23:15:52 +0000

    Completed Task 3

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -10,7 +10,7 @@
 class Vehicle;
 
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
+// COMPLETE FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
@@ -23,12 +23,12 @@ public:
     void send(T &&msg);
 
 private:
-    // std::deque _queue;
+    std::deque<TrafficLightPhase> _queue;
     std::condition_variable _condition;
     std::mutex _mutex;
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
+// COMPLETE! FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
 // The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
